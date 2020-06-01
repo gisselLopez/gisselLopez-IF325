c7fec3b0ed4fc9b187dcc06740b39fc2c26af3af
gissellopez884@gmail.com <gissellopez884@gmail.com>
2020-05-31 21:57:07 -0500

    factura Sencilla

--- ejercicios Tarea/My Project/Application.Designer.vb
+++ ejercicios Tarea/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.ejercicios_Tarea.OperacionesBasicas
+            Me.MainForm = Global.ejercicios_Tarea.FacturaSencilla
         End Sub
     End Class
 End Namespace
