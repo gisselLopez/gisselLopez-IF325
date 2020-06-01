2ff69c063e664cf7ed73f15757b668fc24a32217
gissellopez884@gmail.com <gissellopez884@gmail.com>
2020-05-31 22:23:57 -0500

    Ciclo for

---
+++ ejercicios Tarea/CicloFor.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CicloFor
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTabla = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.txtMostrar = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(145, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese la tabla que desee 1-10"
+        '
+        'txtTabla
+        '
+        Me.txtTabla.Location = New System.Drawing.Point(173, 64)
+        Me.txtTabla.Name = "txtTabla"
+        Me.txtTabla.Size = New System.Drawing.Size(100, 20)
+        Me.txtTabla.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(329, 61)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Tabla"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(329, 120)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 3
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(338, 166)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 4
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'txtMostrar
+        '
+        Me.txtMostrar.Location = New System.Drawing.Point(173, 106)
+        Me.txtMostrar.Multiline = True
+        Me.txtMostrar.Name = "txtMostrar"
+        Me.txtMostrar.Size = New System.Drawing.Size(100, 159)
+        Me.txtMostrar.TabIndex = 5
+        '
+        'CicloFor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(543, 300)
+        Me.Controls.Add(Me.txtMostrar)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtTabla)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "CicloFor"
+        Me.Text = "CicloFor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTabla As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents txtMostrar As TextBox
+End Class
---
+++ ejercicios Tarea/CicloFor.vb
@@ -0,0 +1,29 @@
+﻿Public Class CicloFor
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Numero As Integer
+
+        Numero = txtTabla.Text
+        txtMostrar.Text = ""
+        If Numero > 10 Then
+            MsgBox("Ingrese una tabla que este entre 1-10")
+        Else
+
+            For i = 1 To 10
+
+                'vbCrLf es para hacer salto en cada multiplicacion de cada numero
+                txtMostrar.Text = txtMostrar.Text & Numero & " * " & i & " = " & Numero * i & vbCrLf
+
+            Next
+        End If
+
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtTabla.Clear()
+        txtMostrar.Clear()
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
